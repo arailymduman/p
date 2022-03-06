84c5d424f460a74506f4b723b0acf8265652c323
arailymduman <99992302+arailymduman@users.noreply.github.com>
2022-03-06 17:52:49 +0600

    Add files via upload

---
+++ home/index.js
@@ -0,0 +1,11 @@
+document.getElementById("mon").innerHTML = "M O N";
+
+document.getElementById("ho").innerHTML = "Home";
+
+document.getElementById("ma").innerHTML = "Main";
+
+document.getElementById("text1").innerHTML = " All about places in Nur-Sultan ";
+
+document.getElementById("myImage").src = "imgg.png";
+
+document.getElementById("dem").innerHTML = "Date : " + Date();
